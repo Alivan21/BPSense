0aa2d6f844a0234f38f164b8b1ffc96232b1ab70
alfanalivan <alfanalivan@gmail.com>
2023-06-29 16:42:39 +0700

    init: update nextjs FE

--- Services/Frontend/app/layout.tsx
+++ Services/Frontend/app/layout.tsx
@@ -1,54 +1,19 @@
-import "@/styles/globals.css"
-import { Metadata } from "next"
+import "../styles/global.css";
+import { Montserrat } from "next/font/google";
 
-import { siteConfig } from "@/config/site"
-import { fontSans } from "@/lib/fonts"
-import { cn } from "@/lib/utils"
-import { SiteHeader } from "@/components/site-header"
-import { TailwindIndicator } from "@/components/tailwind-indicator"
-import { ThemeProvider } from "@/components/theme-provider"
+const montserrat = Montserrat({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: {
-    default: siteConfig.name,
-    template: `%s - ${siteConfig.name}`,
-  },
-  description: siteConfig.description,
-  themeColor: [
-    { media: "(prefers-color-scheme: light)", color: "white" },
-    { media: "(prefers-color-scheme: dark)", color: "black" },
-  ],
-  icons: {
-    icon: "/favicon.ico",
-    shortcut: "/favicon-16x16.png",
-    apple: "/apple-touch-icon.png",
-  },
-}
+const bodyClass = `flex min-h-screen flex-col items-center justify-between bg-gray-100 ${montserrat.className}`;
 
-interface RootLayoutProps {
-  children: React.ReactNode
-}
+export const metadata = {
+  title: "BPSense",
+  description: "Aplikasi untuk men-verifikasi pegawai BPS",
+};
 
-export default function RootLayout({ children }: RootLayoutProps) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <>
-      <html lang="en" suppressHydrationWarning>
-        <head />
-        <body
-          className={cn(
-            "min-h-screen bg-background font-sans antialiased",
-            fontSans.variable
-          )}
-        >
-          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-            <div className="relative flex min-h-screen flex-col">
-              <SiteHeader />
-              <div className="flex-1">{children}</div>
-            </div>
-            <TailwindIndicator />
-          </ThemeProvider>
-        </body>
-      </html>
-    </>
-  )
+    <html lang="en">
+      <body className={bodyClass}>{children}</body>
+    </html>
+  );
 }
